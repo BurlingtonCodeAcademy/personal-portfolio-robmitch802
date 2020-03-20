f049364c92a7de783080c54665feaec266191cfb
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-03-20 14:41:57 -0400

    small changes

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -9,7 +9,7 @@ class AboutMe extends React.Component {
             <div className="container">
                 <h1>About Me</h1>
                 <div><img alt="barnyard, two boys, one younger, older one holding chicken" src="/rob04.jpg" /></div>
-                <p>I'm a native Vermonter.(That's me on the left)</p>
+                <p>I'm a native Vermonter. (That's me on the left)</p>
                 <p></p>
                 <Link to={'/'} ><button>Home</button></Link>
             </div>
